13f342e9e13968c89ebbf953f68e8458ec978266
RisaTokuyasu <osanm923108@gmail.com>
2022-08-19 11:52:18 +0900

    deviseのviewを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parmeteres, if: :devise_controller?
+
+    protected
+    def configure_permitted_parmeteres
+        devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users/index'
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_scope :user do
     get 'signin' => 'devise_token_auth/sessions#new'
@@ -8,5 +8,13 @@ Rails.application.routes.draw do
     post 'signup' => 'users#create'
     put 'update' => 'users#update'
   end
+  
   root 'pages#home'
+
+ devise_for :users, controllers: {
+   confirmations: 'users/confirmations',
+   passwords:     'users/passwords',
+   registrations: 'users/registrations',
+   sessions:      'users/sessions',
+ }
 end
